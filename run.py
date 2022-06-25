a31c5485772b1fe4e849fec5b718690ecb53ffdf
Tanvir Ahmed <tanvir66288@gmai.com>
2022-06-25 14:00:08 +0600

    TA | made run.py platform independent

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import sys
 from image_art import ImageArt
+import platform
 
 
 if __name__ == "__main__":
-    file_path = 'images/flower_keep.jpg'
+    file_path = 'images'+("\\" if platform.system() == "Windows" else "/")+'flower_keep.jpg'
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
 
